a0e1eee592763f72556164c54ee4b35dc048996b
HTMLToronto <matthew@htmltoronto.ca>
2013-11-10 10:14:21 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var io = require('socket.io').listen(57800);
 
 io.sockets.on('connection', function (socket) {
 	socket.on('submitUpdateLayout', function (updateData) {
-		console.log(updateData);
 		io.sockets.emit('updateLayout', updateData);
 	});
 });
